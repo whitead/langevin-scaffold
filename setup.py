f72d101d6d87a4f1962b79159dcc0d1e32eaa763
Andrew White <white.d.andrew@gmail.com>
2017-09-18 11:42:43 -0400

    Made package installable

---
+++ setup.py
@@ -0,0 +1,9 @@
+
+from distutils.core import setup
+
+setup(name='LangevinSim',
+      version='0.01',
+      description='Langevin Simulator',
+      author='Andrew White',
+      packages=['lans']
+     )
